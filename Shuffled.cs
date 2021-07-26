dff38b5be70d37ab80bf8a7ce63d450e9278043c
hp <hp@DESKTOP-E81J3KJ>
2021-07-26 16:21:46 +0100

    first commit

---
+++ Shuffled.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekEndTask
+{// Question: Write a query that shuffles a sorted List of words.
+    public static class Shuffled
+    {
+        public static void SortedList()
+        {
+            //A list of word sorted in alphabatical order
+            var SortedList = new List<string>()
+            { "Apple", "Ball", "Cat", "Dog", "Fish", "House","Zebra" };
+
+            Console.WriteLine();
+            //Shuffled the alphabatical sortedlist using the random()
+            Console.WriteLine("Random sorting ===========================");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            var random = new Random();
+            var shuffle = SortedList.OrderBy(x => random.Next());
+            foreach (var items in shuffle)
+            {
+                Console.WriteLine($"{items}");
+            }
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+            //Shuffled the alphabatical sortedlist using the Query Syntax()
+            Console.WriteLine("Query Syntax ===========================");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            var querySortedList = from sorted in SortedList
+                                  orderby random.Next()
+                                  select sorted;
+            foreach (var items in querySortedList)
+            {
+                Console.WriteLine($"{items}");
+            }
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+            //Shuffled the alphabatical sortedlist using the Method Syntax ()
+            Console.WriteLine("Method Syntax ===========================");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            var methodSortedList = SortedList.OrderBy(x => random.Next());
+            foreach (var items in methodSortedList)
+            {
+                Console.WriteLine($"{items}");
+            }
+            Console.ResetColor();
+
+           
+        }
+    }
+}      
+      
+   
+    
+            
+
