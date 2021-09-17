6bec4f3f2ad9a2f7bcd68adebd70db7b8b20ed8a
chinenye ozoagudike <chinenyeozoagudike@gmail.com>
2021-09-17 10:58:02 +0100

    building a sorted list of words

--- Shuffled.cs
+++ Shuffled.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WeekEndTask
 {// Question: Write a query that shuffles a sorted List of words.
